a1aad4c1185b887ae04759d7ac91c9dbf5379a23
LallieDragon <c.simecheck89@yahoo.com>
2019-08-24 12:55:41 -0500

    testing constructor replacement and playing with lifecycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,30 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 //Geolocation API developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
 
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this.state = {
+  //     lat: null,
+  //     errorMessage: ''
+  //   };
+  // }
 
-    this.state = { lat: null };
+  //Line 18 is a complete and equivalent way of doing linkes 8-15
+  state = { lat: null, errorMessage: ''}
 
+  componentDidMount = () => {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({
-          lat: position.coords.latitude
-        })
-      },
-      (error) => console.log(error)
+      position => this.setState({ lat: position.coords.latitude }),
+      error => this.setState({ errorMessage: error.message })
     );
   }
 
-  render() {
-    return (
-      <div>Latitude: { this.state.lat } </div>
+  componentDidUpdate() {
+    console.log("Component updated")
+  }
 
-    )
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading...</div>
   }
 }
 ReactDOM.render(<App />, document.querySelector('#root'))
